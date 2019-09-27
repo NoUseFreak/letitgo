f2294bccf096f6624e7a6ecc3095f7dd0a6e217d
Dries De Peuter <dries@nousefreak.be>
2019-09-28 00:23:24 +0200

    Add release flow

---
+++ scripts/homebrew.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# This script update homebrew tap
+
+# Get the parent directory of where this script is.
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
+
+cd "$DIR"
+
+go run cmd/release/main.go homebrew $(git describe --tags --abbrev=0)
