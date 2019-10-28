0296e9a415367781d1020d5095c8a25705458b32
Dries De Peuter <dries@nousefreak.be>
2019-10-28 10:13:37 +0100

    Fix typo

--- internal/app/action/actions.go
+++ internal/app/action/actions.go
@@ -6,5 +6,5 @@ type Actions []Action
 // Len returns the length of the slice.
 func (s Actions) Len() int { return len(s) }
 
-// Swap swaps two items from possition.
+// Swap swaps two items from position.
 func (s Actions) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
